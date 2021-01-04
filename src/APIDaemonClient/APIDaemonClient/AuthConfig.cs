0d3eefa90aba8455a6fbb559fa789bca4f99bd0d
pmccullough060 <pmccullough060@gmail.com>
2021-01-04 21:49:08 +0000

    first commit

---
+++ src/APIDaemonClient/APIDaemonClient/AuthConfig.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace APIDaemonClient
+{
+    public class AuthConfig
+    {
+        public string Instance { get; set; } = "https://login.microsoftonline.com/{0}";
+        public string TenantId { get; set; }
+        public string ClientId { get; set; }
+        public string Authority
+        {
+            get
+            {
+                return String.Format(CultureInfo.InvariantCulture, Instance, TenantId);
+            }
+        }
+        public string ClientSecret { get; set; }
+        public string BaseAddress { get; set; }
+        public string ResourceId { get; set; }
+
+        public AuthConfig()
+        {
+            //Retrieving the user secrets.
+            var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+            TenantId = config["TenantId"];
+            Instance = config["Instance"];
+            ClientSecret = config["ClientSecret"];
+            ClientId = config["ClientId"];
+            BaseAddress = config["BaseAddress"];
+            ResourceId = config["ResourceId"];
+        }
+    }
+}
---
+++ src/APIDaemonClient/APIDaemonClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace APIDaemonClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var authConfig = new AuthConfig();
+
+            Console.WriteLine($"Authority: {authConfig.Authority}");
+            Console.ReadLine();
+        }
+    }
+}
