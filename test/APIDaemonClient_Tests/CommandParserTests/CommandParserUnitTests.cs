c00780bba531e696005de1dcba19f81f66b01a62
pmccullough060 <pmccullough060@gmail.com>
2021-01-27 11:14:17 +0000

    adding tests to the project

---
+++ test/APIDaemonClient_Tests/CommandParserTests/CommandParserUnitTests.cs
@@ -0,0 +1,33 @@
+﻿using APIDaemonClient;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace APIDaemonClient_Tests.CommandParserTests
+{
+    public class CommandParserUnitTests
+    {
+        public static ILoggerFactory mockLoggerFactory { get; set; }
+        public static ILogger<CommandParser> mockLogger { get; set; }
+
+        public CommandParserUnitTests()
+        {
+            mockLoggerFactory = new NullLoggerFactory();
+            mockLogger = mockLoggerFactory.CreateLogger<CommandParser>();
+        }
+
+        [Fact]
+        public void SomeTest()
+        {
+            var commandParser = new CommandParser(mockLogger);
+
+            var mockObject = new Mock<ITestInterface>();
+
+            commandParser.ConfigureForCLI<ITestInterface>(mockObject.Object);
+        }
+    }
+}
---
+++ test/APIDaemonClient_Tests/CommandParserTests/ITestInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using APIDaemonClient.Attributes;
+
+namespace APIDaemonClient_Tests.CommandParserTests
+{
+    public interface ITestInterface
+    {
+        [CLIMethod("TestMethod_1")]
+        bool TestMethod_1(int number);
+
+
+
+    }
+}
