98378d1cb66e87e9501b1e8b18a3d57ada0a0692
Peter McCullough <47245980+pmccullough060@users.noreply.github.com>
2022-01-11 09:28:13 +0000

    Updated File.IO

--- src/APIDaemonClient/APIDaemonClient/FileIO.cs
+++ src/APIDaemonClient/APIDaemonClient/FileIO.cs
@@ -63,21 +63,19 @@ namespace APIDaemonClient
         public static string CreateNewSettingsFile() //creating a new JObject of settings parameters if the file doesn't already exist....
         {
             var instance = "https://login.microsoftonline.com/{0}";
-            var tenantId = "f50df192-055e-42c9-81eb-37bf51d5bbb6";
+            var tenantId = "tenant_id_goes_here";
 
             dynamic jsonObject = new JObject();
 
             jsonObject.Instance = instance;
             jsonObject.TenantId = tenantId;
-            jsonObject.ClientId = "993ad1fd-2b76-47a7-a584-52bb6cb262d6";
+            jsonObject.ClientId = "client_id_goes_here";
             jsonObject.Authority = String.Format(CultureInfo.InvariantCulture, instance, tenantId);
-            jsonObject.ClientSecret = "IFq.56Dz_bE~~VrqRhGgqm0b8g9Gd35A~e";
+            jsonObject.ClientSecret = "client_secret_goes_here";
             jsonObject.BaseAddress = "https://localhost:5001/api/Commands/1";
-            jsonObject.ResourceId = "api://c0a043f6-4915-4267-8cff-08265fef5a64/.default";
+            jsonObject.ResourceId = "resource";
             jsonObject.LogOutputDirectory = LogFilePath;
             jsonObject.SettingsDirectory = FilePath;
-
-            //jsonObject.list = JToken.FromObject(myList)
 
             return JsonConvert.SerializeObject(jsonObject, Formatting.Indented);
         }
